452d3d9ca5c1b230e038d38064b3574f974d747b
dylantheys <dylan.theys31@gmail.com>
2022-01-05 11:05:48 +0100

    Closing modal function done

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,24 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll(".close");
+
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+
+// close modal event
+closeBtn.forEach((close) => close.addEventListener("click", closeModal));
+
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
